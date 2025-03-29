7c559481058599bd9cb6abcdc3dda618466a788f
veasnapovsr <veasnavarapov@gmail.com>
2025-03-29 15:17:21 +0700

    main_update

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,23 @@
-// import 'package:flutter/material.dart';
-// import 'repository/mock/mock_locations_repository.dart';
-// import 'repository/mock/mock_rides_repository.dart';
-// import 'service/locations_service.dart';
-// import 'service/rides_service.dart';
-//
-// import 'repository/mock/mock_ride_preferences_repository.dart';
-// import 'ui/screens/ride_pref/ride_pref_screen.dart';
-// import 'service/ride_prefs_service.dart';
-// import 'ui/theme/theme.dart';
-//
-// void main() {
-//   // 1 - Initialize the services
-//   RidePrefService.initialize(MockRidePreferencesRepository());
-//   LocationsService.initialize(MockLocationsRepository());
-//   RidesService.initialize(MockRidesRepository());
-//
-//   // 2- Run the UI
-//   runApp(const MyApp());
-// }
-//
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       theme: appTheme,
-//       home: Scaffold(body: RidePrefScreen()),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-
-import 'Provider/ride_prefs_provider.dart';
-import 'repository/mock/mock_locations_repository.dart';
-import 'repository/mock/mock_rides_repository.dart';
+import 'package:week_3_blabla_project/data/local/local_ride_preferences_repository.dart';
+import 'package:week_3_blabla_project/ui/providers/rides_pref_provider.dart';
+import 'data/repository/mock/mock_locations_repository.dart';
+import 'data/repository/mock/mock_rides_repository.dart';
 import 'service/locations_service.dart';
 import 'service/rides_service.dart';
-
-import 'repository/mock/mock_ride_preferences_repository.dart';
+import 'package:provider/provider.dart';
+import 'data/repository/mock/mock_ride_preferences_repository.dart';
 import 'ui/screens/ride_pref/ride_pref_screen.dart';
 import 'service/ride_prefs_service.dart';
 import 'ui/theme/theme.dart';
 
 void main() {
-  // 1 - Initialize the services (keeping existing initialization)
-  RidePrefService.initialize(MockRidePreferencesRepository());
+  // 1 - Initialize tche services
+  // RidePrefService.initialize(MockRidePreferencesRepository());
   LocationsService.initialize(MockLocationsRepository());
   RidesService.initialize(MockRidesRepository());
 
-  // 2- Run the UI with MultiProvider
+  // 2- Run the UI
   runApp(const MyApp());
 }
 
@@ -62,18 +27,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (context) => RidesPreferencesProvider(
-              MockRidePreferencesRepository()
-          ),
-        ),
-      ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: appTheme,
-        home: Scaffold(body: RidePrefScreen()),
-      ),
-    );
+        providers: [
+          ChangeNotifierProvider<RidesPrefProvider>(
+              create: (context) => RidesPrefProvider(
+                  repository: LocalRidePreferencesRepository())),
+        ],
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: appTheme,
+          home: Scaffold(body: RidePrefScreen()),
+        ));
   }
 }
